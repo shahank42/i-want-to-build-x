d2c8a2b18b5fe9587bd47992165652fac91701bd
shahank42 <crazykavlogs@gmail.com>
2025-12-05 22:36:37 +0000

    chore: setup prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import prisma from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany({});
+  console.log(posts);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
